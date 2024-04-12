c6ac524934e9ad415281ed8b1913dc61e876acc7
Veronika Deiko <deykonika2005@gmail.com>
2024-04-12 21:28:13 +0300

    added css for login page

---
+++ BeautySpaceInfrastructure/ViewModel/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeautySpaceInfrastructure.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Введіть електронну пошту клієнта")]
+        [Display(Name = "Email")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Невірний формат електронної пошти")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Введіть пароль")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запам'ятати?")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+    }
+}
+
+
